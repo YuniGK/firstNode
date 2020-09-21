95394e062bf3f5ee79778aa326bec7425e31aa19
YuniGK <1106qwe@hanmail.net>
2020-09-22 00:18:17 +0900

    오류 수정

--- server/index.js
+++ server/index.js
@@ -119,6 +119,10 @@ app.get('/api/users/auth', auth, (req, res) => {
 
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send('hello')
+})
+
 app.get('/api/users/logout', auth, (req, res) => {
   User.findOneAndUpdate({_id:req.user._id},
     {token : ""}, 
